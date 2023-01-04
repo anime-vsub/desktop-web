c8c7283ea4e4e388baa466f428d7a7ba1728824d
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2023-01-04 05:07:59 +0000

    leak memory

--- src/composibles/useFirestore.ts
+++ src/composibles/useFirestore.ts
@@ -105,7 +105,7 @@ export function useFirestore<T extends DocumentData>(
     }
   }
 
-  watch(refOfDocRef, run)
+  // watch(refOfDocRef, run)
 
   if (autoDispose) {
     tryOnScopeDispose(() => {
