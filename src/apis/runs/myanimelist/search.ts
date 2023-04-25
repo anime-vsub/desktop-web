0f19f59413cd35695360218562bc3f6ff8e733ad
Tachibana Shin <tachibshin@duck.com>
2023-04-25 09:13:03 +0000

    split to shared search engine

--- src/apis/runs/myanimelist/search.ts
+++ src/apis/runs/myanimelist/search.ts
@@ -76,10 +76,11 @@ function sliceOthername(othername: string[], size: number, max: number) {
 
 const rPrefixSeason = new RegExp(`${PREFIX_SEASON} (\\d+)`, "gi")
 const rBeforeSeason = new RegExp(`(\\d+)${BEFORE_SEASON}\\s+(?:season)?`, "gi")
-export async function getAmimeMyAnimeList(
+export function searchShared<T extends any[]>(
   name: string,
-  othername: string
-): Promise<ReturnType<typeof MyAnimeListSearchParser>[0]> {
+  othername: string,
+  fetcher: (keyword: string) => Promise<T>
+): Promise<T[0]> {
   const nameRmd = name
     .replace(rPrefixSeason, " $1")
     .replace(rBeforeSeason, "$1")
@@ -100,90 +101,98 @@ export async function getAmimeMyAnimeList(
 
   const keyword = query
   console.log({ keyword }, keyword.length)
-  try {
-    return runRawSearch(keyword).then((anime) => {
-      // console.log(keyword)
-      // console.dir(data, { depth: null })
-      // eslint-disable-next-line functional/no-throw-statement
-      if (anime.length === 0) throw new Error("not_found")
 
-      const ss = name.match(defRegExpGetSeason)?.slice(1).filter(Boolean).at(-1)
-      const regexpSeasonSS = createRegExpGetSeason(ss ?? "\\d+")
+  return fetcher(keyword).then((anime) => {
+    // console.log(keyword)
+    // console.dir(data, { depth: null })
+    // eslint-disable-next-line functional/no-throw-statements
+    if (anime.length === 0) throw new Error("not_found")
 
-      name = name.toLowerCase().trim()
-      const nameRmdSeason = ss
-        ? name.replace(defRegExpGetSeason, "").trim()
-        : name
-      const matchByName = anime.find((item) => {
-        const itemName = ss
-          ? item.name.replace(defRegExpGetSeason, "").toLowerCase().trim()
-          : item.name.toLowerCase()
+    const ss = name.match(defRegExpGetSeason)?.slice(1).filter(Boolean).at(-1)
+    const regexpSeasonSS = createRegExpGetSeason(ss ?? "\\d+")
 
-        if (name === itemName || nameRmdSeason === itemName) {
-          return ss
-            ? !!item.name.match(regexpSeasonSS)
-            : !item.name.match(regexpSeasonSS)
-        }
+    name = name.toLowerCase().trim()
+    const nameRmdSeason = ss
+      ? name.replace(defRegExpGetSeason, "").trim()
+      : name
+    const matchByName = anime.find((item) => {
+      const itemName = ss
+        ? item.name.replace(defRegExpGetSeason, "").toLowerCase().trim()
+        : item.name.toLowerCase()
 
-        return false
-      })
-      if (matchByName) return matchByName
+      if (name === itemName || nameRmdSeason === itemName) {
+        return ss
+          ? !!item.name.match(regexpSeasonSS)
+          : !item.name.match(regexpSeasonSS)
+      }
 
-      const othernameRmdSeason = ss
-        ? othername
-            .split(",")
-            .map((name) => {
-              return name
-                .trim()
-                .replace(defRegExpGetSeason, "")
-                .toLowerCase()
-                .trim()
-            })
-            .join(",")
-        : othername.toLowerCase().trim()
-      const matchByOthername = anime.find((item, id) => {
-        const itemName = ss
-          ? item.name.replace(defRegExpGetSeason, "").toLowerCase().trim()
-          : item.name.toLowerCase()
-        // console.log(item.name, defRegExpGetSeason, itemName)
-
-        if (basicExactOthername(itemName, othernameRmdSeason)) {
-          return ss
-            ? !!item.name.match(regexpSeasonSS)
-            : !item.name.match(regexpSeasonSS)
-        }
-
-        return false
-      })
-
-      if (matchByOthername) return matchByOthername
-
-      const miniSearch = new MiniSearch({
-        fields: ["name_lower"], // fields to index for full-text search
-        storeFields: Object.keys(anime[0]), // fields to return with search results
-      })
-
-      anime.forEach((item) => {
-        item.name_lower = item.name.trim().toLowerCase()
-      })
-      // Index all documents
-      miniSearch.addAll(anime)
-
-      const result = miniSearch.search(otherRmd)[0] as unknown as Awaited<
-        ReturnType<typeof runRawSearch>
-      >[0]
-      console.log(otherRmd, result)
-      // console.log(otherRmd, result.slice(0, 10))
-      // .filter((item) => {
-      //   return ss
-      //     ? !!item.name.match(regexpSeasonSS)
-      //     : !item.name.match(regexpSeasonSS)
-      // }) as unknown as Awaited<ReturnType<typeof runRawSearch>>[0]
-      if (result) return result
-
-      // eslint-disable-next-line functional/no-throw-statement
-      throw new Error("not_found")
+      return false
     })
+    if (matchByName) return matchByName
+
+    const othernameRmdSeason = ss
+      ? othername
+          .split(",")
+          .map((name) => {
+            return name
+              .trim()
+              .replace(defRegExpGetSeason, "")
+              .toLowerCase()
+              .trim()
+          })
+          .join(",")
+      : othername.toLowerCase().trim()
+    const matchByOthername = anime.find((item, id) => {
+      const itemName = ss
+        ? item.name.replace(defRegExpGetSeason, "").toLowerCase().trim()
+        : item.name.toLowerCase()
+      // console.log(item.name, defRegExpGetSeason, itemName)
+
+      if (basicExactOthername(itemName, othernameRmdSeason)) {
+        return ss
+          ? !!item.name.match(regexpSeasonSS)
+          : !item.name.match(regexpSeasonSS)
+      }
+
+      return false
+    })
+
+    if (matchByOthername) return matchByOthername
+
+    const miniSearch = new MiniSearch({
+      fields: ["name_lower"], // fields to index for full-text search
+      storeFields: Object.keys(anime[0]), // fields to return with search results
+    })
+
+    anime.forEach((item) => {
+      item.name_lower = item.name.trim().toLowerCase()
+    })
+    // Index all documents
+    miniSearch.addAll(anime)
+
+    const result = miniSearch.search(
+      otherRmd
+    )[0] as unknown as (typeof anime)[0]
+    console.log(otherRmd, result)
+    // console.log(otherRmd, result.slice(0, 10))
+    // .filter((item) => {
+    //   return ss
+    //     ? !!item.name.match(regexpSeasonSS)
+    //     : !item.name.match(regexpSeasonSS)
+    // }) as unknown as Awaited<ReturnType<typeof runRawSearch>>[0]
+    if (result) return result
+
+    // eslint-disable-next-line functional/no-throw-statements
+    throw new Error("not_found")
+  })
+}
+
+export async function getAmimeMyAnimeList(
+  name: string,
+  othername: string
+): Promise<ReturnType<typeof MyAnimeListSearchParser>[0]> {
+  try {
+    return searchShared(name, othername, runRawSearch)
   } catch (err) {
     if ((err as Error | null)?.message?.startsWith("Unexpected token ")) {
       // limit request
@@ -191,7 +200,6 @@ export async function getAmimeMyAnimeList(
       return getAmimeMyAnimeList(name, othername)
     }
 
-    // eslint-disable-next-line functional/no-throw-statement
     throw err
   }
 }
