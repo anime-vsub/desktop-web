2faf6e1b5ff1648317ff97dd10a0531e2fc9fd75
Tachibana Shin <tachibshin@duck.com>
2024-03-29 18:12:56 +0000

    update package  `animevsub-download-manager`

--- src/stores/adm.ts
+++ src/stores/adm.ts
@@ -1,63 +1,20 @@
 import FS from "@isomorphic-git/lightning-fs"
 import { AnimeDownloadManager } from "animevsub-download-manager/src/main"
+import { get, getMany, set, setMany } from "idb-keyval"
 import { defineStore } from "pinia"
 
 export const useADM = defineStore("adm", () => {
   const fs = new FS("adm").promises
-  async function mkdirRecursive(path: string) {
-    try {
-      await fs.mkdir(path)
-    } catch (err: any) {
-      if (err?.code === "ENOENT")
-        await mkdirRecursive(path.split("/").slice(0, -1).join("/"))
-      else throw err
-    }
-  }
 
   const tasks = shallowReactive(new Map())
 
   const adm = markRaw(
     new AnimeDownloadManager(
       {
-        readdir(path) {
-          return fs.readdir(path)
-        },
-        readFile(path) {
-          return fs.readFile(path, "utf8") as Promise<string>
-        },
-        readFiles(paths: string[]) {
-          return Promise.all(
-            paths.map((path) => fs.readFile(path, "utf8") as Promise<string>)
-          )
-        },
-        hasFile(path: string) {
-          return fs
-            .lstat(path)
-            .then((value) => value.isFile() && { size: value.size })
-            .catch(() => false)
-        },
-        async writeFile(path: string, content: string | Uint8Array) {
-          await fs.writeFile(path, content).catch((err) => {
-            if (err.code === "ENOENT") {
-              return mkdirRecursive(
-                path.split("/").slice(0, -1).join("/")
-                // eslint-disable-next-line promise/no-nesting
-              ).then(() => this.writeFile(path, content))
-            }
-
-            throw err
-          })
-        },
-        async writeFiles(
-          contents: [string, string | Uint8Array][]
-        ): Promise<void> {
-          await Promise.all(
-            contents.map(([path, content]) => this.writeFile(path, content))
-          )
-        },
-        async unlinks(paths: string[]): Promise<void> {
-          await Promise.all(paths.map((path) => fs.unlink(path)))
-        }
+        get,
+        set,
+        getMany,
+        setMany
       },
       {
         request(uri: string, method = "GET"): Promise<Response> {
