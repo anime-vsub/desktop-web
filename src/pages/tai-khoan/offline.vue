a0b78be58794ec822a2c2c3f29a0a0899a0b5e81
Tachibana Shin <tachibshin@duck.com>
2024-03-30 05:35:50 +0000

    update page offline

--- src/pages/tai-khoan/offline.vue
+++ src/pages/tai-khoan/offline.vue
@@ -10,52 +10,24 @@
 
       <div v-else class="row">
         <div
+          v-for="[_, task] in admStore.tasks"
+          :key="task.seasonInfo.seasonId"
+          class="col-6 col-sm-4 col-md-3"
+        >
+          <CardOffline :data="task.seasonInfo">
+            <div
+              class="absolute w-full h-full top-0 left-0 bg-dark bg-opacity-20"
+            >
+              {{ getPerPacento(task.episodes) }}%
+            </div>
+          </CardOffline>
+        </div>
+        <div
           v-for="item in seasons"
           :key="item.seasonId"
           class="col-6 col-sm-4 col-md-3"
         >
-          <router-link :to="`/phim/${item.seasonId}`" class="mt-1 mb-4">
-            <div class="w-full">
-              <q-img-file
-                no-spinner
-                :src="item.poster"
-                referrerpolicy="no-referrer"
-                :ratio="1920 / 1080"
-                class="!rounded-[4px]"
-              >
-                <BottomBlur class="px-0 h-[40%]">
-                  <div
-                    class="absolute bottom-0 left-0 z-10 w-full min-h-0 py-2 px-2 flex items-center"
-                  >
-                    <Quality>{{ item.quality }}</Quality>
-                    <q-separator vertical class="mx-1" />
-                    {{ item.seasonOf?.name ?? item.yearOf }}
-                  </div>
-                </BottomBlur>
-              </q-img-file>
-            </div>
-
-            <div class="mt-2 px-1">
-              <h2 class="text-16px font-weight-medium line-clamp-2 mb-2">
-                {{ item.name }}
-              </h2>
-
-              <p class="text-gray-300 text-12px">{{ item.currentSeason?.name }}</p>
-              <p class="text-gray-300 text-12px flex items-center truncate">
-                {{
-                  $t("formatview-data-views-luot-xem", [formatView(item.views)])
-                }}
-                <span class="mx-2">&bull;</span> <Star /> {{ item.rate }}
-              </p>
-
-              <span
-                v-for="contri in item.contries"
-                :key="contri.path"
-                class="rounded-md bg-gray-700 bg-opacity-50 px-2 py-0.5 mx-1 leading-normal mt-1 inline-block text-12px text-gray-400"
-                >{{ contri.name }}</span
-              >
-            </div>
-          </router-link>
+          <CardOffline :data="item" />
         </div>
       </div>
     </template>
@@ -65,25 +37,14 @@
 
 <script lang="ts" setup>
 import { useHead } from "@vueuse/head"
-import BottomBlur from "components/BottomBlur.vue"
-import QImgCustom from "components/QImgCustom"
-import QImgFile from "components/QImgFile.vue"
 import ScreenError from "components/ScreenError.vue"
 import ScreenLoading from "components/ScreenLoading.vue"
 import ScreenNotFound from "components/ScreenNotFound.vue"
-import { QInfiniteScroll } from "quasar"
-import { forceHttp2 } from "src/logic/forceHttp2"
-import { parseChapName } from "src/logic/parseChapName"
-import { parseTime } from "src/logic/parseTime"
-import { useHistoryStore } from "stores/history"
 import { computed } from "vue"
 import { useI18n } from "vue-i18n"
 import { useRequest } from "vue-request"
-import GridCard from "components/GridCard.vue"
-import { computedAsync } from "@vueuse/core"
-import { formatView } from "src/logic/formatView"
-import Star from "components/Star.vue"
-import Quality from "components/Quality.vue"
+import CardOffline from "components/offline/CardOffline.vue"
+import { Episode } from "animevsub-download-manager"
 
 const { t } = useI18n()
 const admStore = useADM()
@@ -118,4 +79,14 @@ const {
 } = useRequest(() => {
   return admStore.adm.listSeason("asc")
 })
+
+function getPerPacento(episodes: Map<string, { cur: number; total: number }>) {
+  let cur = 0
+  let total = 0
+  for (const [_, meta] of episodes) {
+    cur += meta.cur
+    total += meta.total
+  }
+  return Math.round((cur / total) * 100)
+}
 </script>
