0ec296dd0847d5dffb6122347f3aae80128a2b8d
Tachibana Shin <tachibshin@duck.com>
2024-04-30 17:05:11 +0000

    chore: lint

--- src/logic/get-redirect.ts
+++ src/logic/get-redirect.ts
@@ -4,7 +4,7 @@ export function getRedirect(req: Request): Promise<string> {
   return new Promise((resolve, reject) => {
     fetch(req.url, { ...req, signal: control.signal })
       .then((res) => {
-        // eslint-disable-next-line promise/always-return
+         
         resolve(res.url)
         control.abort()
       })
