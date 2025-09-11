996f665115cddab6b9034688797b35ea05c91043
Fwy13_ <136143802+fwy13@users.noreply.github.com>
2025-09-11 16:18:46 +0700

    Change the way data is written to the temporary ts segment.
    If I write the original data fetched from fetchFile it won't read the ts segment to combine into mp4, so I split the image and ts segment. Then write the ts segment and it works.

--- src/logic/convert-hls-to-mp4.ts
+++ src/logic/convert-hls-to-mp4.ts
@@ -66,7 +66,12 @@ export async function convertHlsToMP4(
       limit(() =>
         retryAsync<void>(async () => {
           const path = `${hash}-${i}.ts`
-          await ffmpeg.writeFile(path, await fetchFile(segment.uri))
+          let response = await fetchFile(segment.uri);
+          response =
+            indexOfIEND(response) !== 1
+              ? response.slice(indexOfIEND(response))
+              : response;
+          await ffmpeg.writeFile(path, response);
           segment.uri = path
           downloaded++
           timeCurrent += segment.duration
@@ -93,4 +98,19 @@ export async function convertHlsToMP4(
   const mp4Data = await ffmpeg.readFile(`${hash}-output.mp4`)
 
   return mp4Data
+}
+function indexOfIEND(buf: Uint8Array<ArrayBufferLike>): number {
+  for (let i = 0; i < buf.length - IEND_IMAGE.length; i++) {
+    let found = true;
+    for (let j = 0; j < IEND_IMAGE.length; j++) {
+      if (buf[i + j] !== IEND_IMAGE[j]) {
+        found = false;
+        break;
+      }
+    }
+    if (found) {
+      return i + IEND_IMAGE.length;
+    }
+  }
+  return -1;
 }
